f042e36ab061bbb802105cc2f5e4bbb59bc27046
Andy-Ta <me.andyta@gmail.com>
2020-03-16 14:28:31 -0400

    Initial commit

---
+++ src/main/java/com/algonquincollege/cst8277bank/Cst8277BankApplication.java
@@ -0,0 +1,13 @@
+package com.algonquincollege.cst8277bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst8277BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst8277BankApplication.class, args);
+	}
+
+}
